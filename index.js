37366649ddc89befe66cd0e41f89cd3cfdbb390c
Harsha Pareek <harshapareek14@gmail.com>
2024-06-25 01:19:29 +0530

    basic layout

---
+++ index.js
@@ -0,0 +1,20 @@
+let menu= document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+};
+const sr= ScrollReveal({
+    origin:'top',
+    distance:'85px',
+    duration:2500,
+    reset:true
+})
+sr.reveal('.home-text',{delay:300});
+sr.reveal('.home-img',{delay:400});
+sr.reveal('.about',{delay:400});
